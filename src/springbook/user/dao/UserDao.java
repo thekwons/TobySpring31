3fd5602409c0279bcec350508f9cdad783a7519b
Sungwook Kwon <swooki@gmail.com>
2014-03-17 19:14:45 -0400

    p59 Working but helpless code

---
+++ src/springbook/user/dao/UserDao.java
@@ -0,0 +1,67 @@
+package springbook.user.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import springbook.user.domain.User;
+
+public class UserDao {
+	public void add(User user) throws ClassNotFoundException, SQLException {
+		Class.forName("org.postgresql.Driver");
+		Connection c = DriverManager.getConnection(
+				"jdbc:postgresql://localhost/testdb", "postgres", "");
+		PreparedStatement ps = c
+				.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+		ps.setString(1, user.getId());
+		ps.setString(2, user.getName());
+		ps.setString(3, user.getPassword());
+
+		ps.execute();
+
+		ps.close();
+		c.close();
+	}
+
+	public User get(String id) throws ClassNotFoundException, SQLException {
+		Class.forName("org.postgresql.Driver");
+		Connection c = DriverManager.getConnection(
+				"jdbc:postgresql://localhost/testdb", "postgres", "");
+		PreparedStatement ps = c
+				.prepareStatement("select * from users where id=?");
+		ps.setString(1, id);
+
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		User user = new User();
+		user.setId(rs.getString("id"));
+		user.setName(rs.getString("name"));
+		user.setPassword(rs.getString("password"));
+
+		rs.close();
+		ps.close();
+		c.close();
+
+		return user;
+	}
+	
+	public static void main(String [] args)throws ClassNotFoundException, SQLException {
+		UserDao dao = new UserDao();
+		User user = new User();
+		user.setId("whiteship");
+		user.setName("Sungwook Kwon");
+		user.setPassword("married");
+		
+		dao.add(user);
+		
+		System.out.println(user.getId() + " was successfully added");
+		
+		User user2 = dao.get(user.getId());
+		System.out.println(user2.getName());
+		System.out.println(user2.getPassword());
+		
+		System.out.println(user2.getId() + " was successfully retrieved");
+	}
+}
